757bd88dbc56e8dcfbf6d54906bcf32023cf2af7
DiegoVsp <diego-vespa2020@outlook.com.br>
2020-12-21 19:36:53 -0300

    exercicios att

--- exercicios.js
+++ exercicios.js
@@ -1,5 +1,59 @@
 // 1. declare uma variável de nome weight
 
 let weight = 49.2
-
+// 2. Que que tipo de dado é a variável acima?
 console.log(typeof weight)
+
+// 3. Declare uma variável e atribua valores para cada um dos dados:
+let name = "Diego"
+let age = 29
+let stars = 4.5
+let isSubscribed = true
+
+console.log(`Nome ${name}, age ${age}, stars ${stars}, isSubscribed ${isSubscribed}`)
+
+/**
+ * 4. A variável student é de que tipo de dados? 
+ * 4.1 Atribua a ela as mesmas propriedades e valores do exercicio 3.
+ * 4.2 Mostre no console a seguinte mensagem
+ * <name> de idade <age> pesa <weight> kilos.
+ */
+let student = {}
+
+console.log(typeof student)
+
+student = {
+  nome: 'Diego',
+  idade: 29,
+  peso: 83.5,
+  stars: 4.5,
+  isSubscribed:true
+}
+console.log(student)
+
+let { nome, idade, peso } = student
+console.log(`${nome} de idade ${idade} pesa ${peso} kilos.`)
+
+/**
+ * 5. Declare uma variável do tipo Array, de nome students e atribua a ela
+ * nenhum valor, ou seja, somente um Array vazio
+ */
+let estudantes = []
+
+// estudantes.push(student)
+estudantes = [student]
+console.log(estudantes)
+console.log(estudantes[0])
+
+const mario = {
+  nome:"Mario",
+  idade:26,
+  peso:88.4,
+  isSubscribed:false
+}
+
+estudantes[1] = mario
+console.log(estudantes)
+
+console.log(a)
+var a = 1
